69f1a33b82591759a899df28490d9df0668dfb70
Rodrigo Montes <rjmontes@gmail.com>
2020-03-19 13:40:13 -0300

    Video 126 - Labels
    - Added label (text mesh) to identify coordinates of each cube. The label auto updates on the snap script (renamed to CubeEditor.cs)

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[ExecuteInEditMode]     //to add functionality executable on the edit mode
+[SelectionBase]         //to force the selection of the object this script is attached to when selecting any part of the object on the editor
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField][Range(1,20)] int gridSize = 10;
+
+    TextMesh textMesh;
 
     // Update is called once per frame
     void Update()
@@ -15,8 +18,9 @@ public class EditorSnap : MonoBehaviour
         snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPosition.y = 0f;                                                            //keep the cube always on the ground
         snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = snapPosition;
-        
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = transform.position.x / gridSize + "," + transform.position.z / gridSize;
     }
 }
