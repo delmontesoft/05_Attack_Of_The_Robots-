5034796d4169e372fdd113796dfcc701e49e4dc5
Rodrigo Montes <rjmontes@gmail.com>
2020-03-19 20:27:48 -0300

    Video 127 - Lists
    - Added Enemy (cylinder) placeholder
    - Added new script for enemy movement
    - Added empty script to blocks to identify them as blocks
    - Upgraded Project Unity version from 2019.3.5f1 to 2019.3.6f1

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -13,14 +13,31 @@ public class CubeEditor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SnapCubeToGrid();
+
+        UpdateCoordText();
+    }
+
+    private void SnapCubeToGrid()
+    {
         Vector3 snapPosition;
 
         snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPosition.y = 0f;                                                            //keep the cube always on the ground
         snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = snapPosition;
+    }
+
+    private void UpdateCoordText()
+    {
+        string coordText;
+
+        coordText = "(" + transform.position.x / gridSize + "," + transform.position.z / gridSize + ")";
+
 
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = transform.position.x / gridSize + "," + transform.position.z / gridSize;
+        textMesh.text = coordText;
+
+        transform.name = coordText;
     }
 }
