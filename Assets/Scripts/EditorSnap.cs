317d72ec74d7a19c9766c3f94e9628021f7e0086
Rodrigo Montes <rjmontes@gmail.com>
2020-03-19 12:56:00 -0300

    Video 125 - Snap by Script
    - Added EditorSnap script to snap the cube prefab by code (help the designer)

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField][Range(1,20)] int gridSize = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPosition;
+
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.y = 0f;                                                            //keep the cube always on the ground
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = snapPosition;
+        
+    }
+}
