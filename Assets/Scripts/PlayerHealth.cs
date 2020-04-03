731612901268ae26ce4a135fbfc072ccf934b605
Rodrigo Montes <rjmontes@gmail.com>
2020-04-03 18:22:00 -0300

    Video 159 - I'm alive!
    - Added player health and decreased it every time an enemy reaches the base
    - Re arranged the level layout and camera

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int playerHealth = 10;
+    [SerializeField] int healthDecrease = 1;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (playerHealth >0)
+        {
+            playerHealth = playerHealth - healthDecrease;
+
+        } else
+        {
+            //TODO do something when player loses
+            print("You lose!");
+        }
+        
+    }
+}
