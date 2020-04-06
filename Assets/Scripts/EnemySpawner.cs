10b8d8307ee489e4a497abc5a6118ba4c63f4a5a
Rodrigo Montes <rjmontes@gmail.com>
2020-04-06 13:33:54 -0400

    Video 161 - Post process
    - Added score and player health UI
    - Added post process effects (a little bit of bloom)

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] [Range(0.5f, 20f)] float secondsBetweenSpawns = 2f;
     [SerializeField] Enemy enemyPrefab;
+    [SerializeField] Text enemyScoreText;
+
+    int enemyScore = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyScoreText.text = enemyScore.ToString();
         StartCoroutine(SpawnEnemy());
     }
 
@@ -18,8 +23,15 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)        //TODO do until some condition (win or lose)
         {
+            AddEnemyScore(); 
             Instantiate(enemyPrefab, transform.position, Quaternion.identity, transform);
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    private void AddEnemyScore()
+    {
+        enemyScore++;
+        enemyScoreText.text = enemyScore.ToString();
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int playerHealth = 10;
     [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText;
+
+    private void Start()
+    {
+        healthText.text = playerHealth.ToString();
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
         if (playerHealth >0)
         {
             playerHealth = playerHealth - healthDecrease;
+            healthText.text = playerHealth.ToString();
 
         } else
         {
